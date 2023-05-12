7fedad324dc6f92b48ccbcf7d457f4877fc21893
leminhman1801 <leminhman1801@gmail.com>
2023-05-12 07:55:52 +0700

    Update header.js

--- header.js
+++ header.js
@@ -1,9 +1,1 @@
-const createHeader = () => {
-  const header = document.createElement("header");
-  const heading = document.createElement("h1");
-  heading.textContent = "Đây là tiêu đề";
-  header.appendChild(heading);
-  return header;
-};
 
-export default createHeader;
